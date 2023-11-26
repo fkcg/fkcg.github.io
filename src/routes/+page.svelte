524eb7a88754dc55572eff184984567211a4d9f3
Mattias Billing Clason <buvese@gmail.com>
2023-11-26 09:07:51 +0100

    Changed membertype to a dropdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     const certLevels = [{value: "A", text: "A"}, {value: "B", text: "B"}, {value: "C", text: "C"}, {value: "D", text: "D"}, {value: "E", text: "E - Elev"}]
+    const memberType = [{value: "Medlem", text: "Medlem"}, {value: "Hedersmedlem", text: "Hedersmedlem"}, {value: "Stödmedlem", text: "Stödmedlem"}]
     let cert = "";
     let certLevel = "A";
     let name = "";
@@ -7,11 +8,13 @@
     let tel = "";
     let nameRel = "";
     let telRel = "";
-    let hmember = false;
+    let member = "Medlem";
     
-    $: swishUrl = "https://app.swish.nu/1/p/sw/?sw=1235425467&amt=" + (hmember ? "2100" : "3000") + "&cur=SEK&msg=" + cert + "&src=qr"
+    $: memberPrice =  (member == "Hedersmedlem" ? "2100" : member == "Stödmedlem" ? "700" : "3000");
 
-    $: postData = "entry.2040625706=" + encodeURIComponent(cert) + "&entry.1366402579=" + encodeURIComponent(name) + "&entry.1208613624=" + encodeURIComponent(email) + "&entry.204702185=" + encodeURIComponent(tel) + "&entry.79715208=" + encodeURIComponent(certLevel) + "&entry.1888244923=" + encodeURIComponent(nameRel) + "&entry.783511263=" + encodeURIComponent(telRel)
+    $: swishUrl = "https://app.swish.nu/1/p/sw/?sw=1235425467&amt=" + memberPrice + "&cur=SEK&msg=" + cert + "&src=qr";
+
+    $: postData = "entry.2040625706=" + encodeURIComponent(cert) + "&entry.1366402579=" + encodeURIComponent(name) + "&entry.1208613624=" + encodeURIComponent(email) + "&entry.204702185=" + encodeURIComponent(tel) + "&entry.79715208=" + encodeURIComponent(certLevel) + "&entry.1888244923=" + encodeURIComponent(nameRel) + "&entry.783511263=" + encodeURIComponent(telRel);
     /**
 	 * @type {any}
 	 */
@@ -19,11 +22,12 @@
 
     async function apply() {
         dialog.showModal();
+        /*
 		const res = await fetch("https://docs.google.com/forms/u/0/d/e/1FAIpQLSeUVFeWKF0aPC-ZzD_hu7-_RA2EwD_DslduLWBJDodc9K-dBQ/formResponse", {
 			method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded"},
 			body: postData
-		})/*
+		}).then((r) => {})
         if (res.ok && res.status >= 200 && res.status < 300) {
             
         } else {
@@ -38,9 +42,9 @@
         <!-- svelte-ignore a11y-no-static-element-interactions -->
         <div on:click|stopPropagation>
             <h1>Ansökan mottagen</h1>
-            <p>För att medlemsskapet skall godkännas så behöver medlemsavgiften på {hmember ? "2100" : "3000"} kr skickas till antingen Swishnummer&nbsp;1235425467 eller Bankgironummer&nbsp;117-0224.</p>
+            <p>För att medlemsskapet skall godkännas så behöver medlemsavgiften på {memberPrice} kr skickas till antingen Swishnummer&nbsp;1235425467 eller Bankgironummer&nbsp;117-0224.</p>
             <p>Ange certnummer som meddelande.</p>
-            <img src={hmember ? "swish_2100.svg" : "swish_3000.svg"} alt="Swish QR Code" width="300px" height="300px" />
+            <img src={"swish_" + memberPrice + ".svg"} alt="Swish QR Code" width="300px" height="300px" />
             <a href={swishUrl}><button>Öppna Swish på denna enhet</button></a>
         </div>
     </dialog>
@@ -61,10 +65,19 @@
     <input id="email" type="email" bind:value={email} placeholder="Email" autocomplete="email" />
     <label for="tel">Telefon:</label>
     <input id="tel" type="tel" bind:value={tel} placeholder="Telefon" autocomplete="tel" />
+    <label for="certlevel">Typ av medlemsskap:</label>
+    <select id="memberType" bind:value={member}>
+		{#each memberType as type}
+            <option id={type.value} value={type.value}>
+				<label for={type.value} class="member">{type.text}</label>
+			</option>
+		{/each}
+	</select>
+    <!--
     <div id="hmember-div">
         <label id="hmember-label" for="hmember">Hedersmedlem:</label>
         <input id="hmember" type="checkbox" bind:checked={hmember}/>
-    </div>
+    </div>-->
     <div id="rel-div">
         <h2>Närmast anhörig</h2>
         <input id="nameRel" type="name" bind:value={nameRel} placeholder="Namn" autocomplete="name" />
@@ -96,26 +109,28 @@
     */
     * {
         color: #000000;
-        font-size: 1em;
+        font-size: 1rem;
         font-family: 'Roboto', sans-serif;
     }
     .centered {
-        max-width: 30em;
+        max-width: 30rem;
         margin: 0 auto;
-        border-radius: 5px;
+        border-radius: 0.5rem;
         background-color: #f2f2f2;
-        padding: 20px;
+        padding: 1rem;
     }
     label {
         width: 100%;
-        padding: 0px 5px;
-        display: inline-block;
+        padding: 0rem 0.25rem;
     }
     #hmember-div {
         display: flex;
+        flex-wrap: wrap;
         justify-content: space-between;
+        align-items: center;
         width: 100%;
         margin: 0px 0px 15px 0px;
+        display: inline-block;
     }
     #hmember-label {
         width: 100%;
@@ -127,6 +142,7 @@
         accent-color: #00DCE7;
         padding: 10px 0px 0px 5px;
         margin: 12px 0px 0px 100px;
+        align-self: center;
     }
     input {
         width: 100%;
@@ -158,7 +174,7 @@
         box-sizing: border-box;
         background-color: #FFFFFF;
     }
-    .level {
+    .level, .member {
         width: 100%;
         padding: 12px 20px;
         margin: 8px 0;
